f8d6255b43d37cdd8a73522ae9503b3f31c6321a
keithos118 <keith.oshea118@gmail.com>
2019-08-04 13:40:48 +0100

    updated to get average price of cars by make

--- com/cars/cars-app.py
+++ com/cars/cars-app.py
@@ -23,6 +23,11 @@ def cars_by_id(id):
    if request.method == 'GET':
        return get_car_by_id(id)
 
+@app.route("/cars/average/<name>", methods = ['GET', 'POST'])
+def cars_average(name):
+   if request.method == 'GET':
+       return get_average_price(name)
+
 def get_cars():
     cars = session.query(Car).all()
     return jsonify(cars=[c.serialize for c in cars])
@@ -30,6 +35,21 @@ def get_cars():
 def get_car_by_id():
     cars = session.query(Car).filter_by(id=id).all()
     return jsonify(cars=[c.serialize for c in cars])
+
+
+def get_average_price(entry):
+    cars = session.query(Car).filter_by(make=entry).all()
+    cars_list = (c.serialize for c in cars)
+
+    price_list = []
+    for value in cars_list:
+        price_list.append(value['price'])
+    average_price = (average(price_list))
+    return jsonify(cars=average_price)
+
+
+def average(data):
+    return sum(data) / len(data)
 
 if __name__ == '__main__':
    app.debug = True
