7efd436b9eead2fbee7e450fa96b9e6d85db3811
keithos118 <keith.oshea118@gmail.com>
2019-08-04 12:54:09 +0100

    added csv import to update database with all entries

--- com/cars/populate_db.py
+++ com/cars/populate_db.py
@@ -9,7 +9,30 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
-carOne = Car(chassis_no= "12345A", make="Nissan", model="Micra", year="1999", last_updated="2017-02-01 00:00:00", price=500.00, id=1)
-session.add(carOne)
-session.commit()
+# carOne = Car(chassis_no= "12345A", make="Nissan", model="Micra", year="1999", last_updated="2017-02-01 00:00:00", price=500.00, id=1)
+# session.add(carOne)
+# session.commit()
 
+import csv
+with open('cars.csv', 'r') as csv_file:
+    csv_reader = csv.reader(csv_file)
+
+    buffer = []
+    for row in csv_reader:
+        if row[6] == "":
+            row[6]+=0
+        buffer.append({
+            'make': row[0],
+            'model': row[1],
+            'year': row[2],
+            'chassis_no': row[3],
+            'id': row[4],
+            'last_updated': row[5],
+            'price': row[6]
+        })
+        if len(buffer) % 10000 == 0:
+            session.bulk_insert_mappings(Car,buffer)
+            buffer = []
+
+    session.bulk_insert_mappings(Car,buffer)
+    session.commit()
